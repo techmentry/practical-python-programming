7b662557107dfff7b99ec57121f4491ae6cc1a64
Sarthak Tickoo <sarthakt@outlook.com>
2022-02-20 12:08:36 -0500

    Material

--- src/day-1/first-python-samples.py
+++ src/day-1/first-python-samples.py
@@ -5,77 +5,91 @@
 
 """
 This is called a multi-line comment in python.
-What this means is that whatever I type between the quoted section no matter how many lines are used is a comment.
+What this means is that whatever I type between the quoted 
+section no matter how many lines are used is a comment.
 """
 
 '''
-This is also called a multi-line comment in python. there is not difference between writing a multi-line comment using double-quotes or single quotes.
+This is also called a multi-line comment in python.
+There is no difference between writing a multi-line comment using 3 double-quotes(")or 3 single quotes (').
 '''
 
 """
-In python, to print something to the console/ result window/ output window, we can use the function print
+In python, to print something to the console/ result window/ output window, we can use print.
 print is a function.
 """
 print("Hello world!")
 
 """
-What is a function?
-Functions are a set or group of instructions.
+What is a function? - Functions are a set or group of instructions.
 
 Calling a function - basically means executing a set of instructions together
 
-E.g.  
+E.g.
 calculate_income_tax(gross_salary, total_deductions):
     # return calculated tax based on the formulas.
-    
+
 income_tax = calculate_income_tax(100000, 4000)
 
 Function has 3 components
 1. Function name - the name that you give to a function to call it/ use it (Mandatory)
-2. Function parameters - allow you to give them (called as passing) values, which it can use and do something (Optional)
-3. Function return value - A function can have a return value based on what the function does (Optional)
+2. Function parameters - allow you to give them (called as passing) values
+                            which it can use and do something (Optional)
+3. Function return value - A function can have a return value
+                            based on what the function does (Optional)
 """
 
 """
-Variables - Are quantities whose values are not fixed, because variables are used to create a generalized logic or algorithm in such a way that no matter what value is present in that variable, the
-output can be determined.
+Variables
+Are quantities whose values are not fixed, because variables are used
+to create a generalized logic or algorithm in such a way that no matter
+what value is present in that variable, the output can be determined.
 """
 
-# in programming languages, variables have a data-type
-# what does a data-type mean? integer, floating point values, strings, char,
 """
-What a data type is telling you is what kind of data does a variable store.
+In programming languages, variables have a data-type
+Examples of data types - integer, floating point values, strings, char,
+
+What does a data-type mean?
+    What a data type is telling you is what kind of data does a variable store.
+    And how should the computer deal with it
 
 Everything that the computer deals with is binary. 
-
 E.g. even this text that I am typing the computer understands this as binary.
 
-ASCII UTF-n encoding
+------------------------------------
+For the computer, the binary number (01011001 or 0x59 or 89 in decimal) can mean anything.
+* if you read it as an int, then it will give me 89
+* if I tell the computer to read this as a character, then it will read it as 'Y'
+* if I tell the computer to read this as something else,
+    then it will it read it as that something which is defined concretely
+
+-- The way you know to read this as
+alphabet Y
+or number
+or something else
+
+is through the data type of the variable
 """
 
-
-"""
-(0101 1001) read as int, then it will give me 89
-if I tell the computer to read this as a character, then it will read it as 'Y'
-if I tell the computer to read this as something else, then it will it read it as that something which is defined concretly
-
-5    9
--- how do you know to read this as alphabet Y, or number (59)decimal or hexadecimal, or something else.
-"""
-
-# define a variable
 '''
-1. You can only use numbers, alphabets, and underscore in your variable name. All other characters like #,$,- are not allowed.
-2. Your variable name needs to start with either an alphabet or an underscore e.g. a, a1, b2, _a, _1 are valid. 1a, 1_a 1_2, 1_ are not valid.
-3. You need to have at least 1 character as a part of your variable name a aa aaaaaaa aaaa_aaaa _aasdfadsfasd adfasdfasdf_ _adsfadsf23423_asdfads_asdf_123
-4. Variable names are case sensitive. i.e. var1, Var1, vAr1, vaR1 are all DIFFERENT variables. They are not the same.
+Rules for defining a variable
+1. You can only use numbers, alphabets, and underscore in your variable name.
+    All other characters like #,$,- are not allowed.
+2. Your variable name needs to start with either an alphabet or an underscore.
+    e.g. a, a1, b2, _a, _1 are valid. 1a, 1_a 1_2, 1_ are not valid.
+3. You need to have at least 1 character as a part of your variable name.
+    e.g. a aa aaaaaaa aaaa_aaaa _aasdfadsfasd adfasdfasdf_ _adsfadsf23423_asdfads_asdf_123 are valid
+4. Variable names are case sensitive. 
+    i.e. var1, Var1, vAr1, vaR1 are all DIFFERENT variables. They are not the same.
 '''
 
 '''
-Snake case.
+We will use Snake case (https://en.wikipedia.org/wiki/Snake_case) for our naming programming style.
 '''
-# Integer
+# Declaring an integer variable
 int_var = 59
+
 # to convert a int or any other value to a string, you need to call the str(value) function with the value.
 print("My first integer variable: " + str(int_var))
 
@@ -91,10 +105,6 @@ print("My name is: " + string_var)
 """
 Basic operations - addition, multiplication, subtraction, division
 """
-"""
-Python is converting 1+2+3 -> an object and then this object gets passed to the print function implicitly.
-Then, this object is converted to string explicitly
-"""
 
 int_sum = 1+2+5+8+9+6
 print("My integer sum is: " + str(int_sum))
@@ -102,11 +112,20 @@ print("My integer sum is: " + str(int_sum))
 frac_sum = 1.05 + 0.006 + 1 + 40.7789
 print("My fractional sum is: " + str(frac_sum))
 
+
+"""
+Python is converting 1+2+3 -> an object and then this object gets passed to the print function implicitly.
+Then, this object is converted to string explicitly
+"""
+print(1+2+3)
+
+# examples of performing operations with variables
 int_var_1 = 22
 int_var_2 = 56
 sum_var = int_var_1 + int_var_2 + 5 + int_sum
 print("My complex integer sum is: " + str(sum_var))
-print("This is an example of directly printing the sum of a variable without storing it in another variable: " + str(int_var_1 + int_var_2 + 5 + int_sum))
+print("This is an example of directly printing the sum of a variable without storing it in another variable: " +
+      str(int_var_1 + int_var_2 + 5 + int_sum))
 
 int_diff = 89-100
 print("My first integer difference is: " + str(int_diff))
@@ -118,7 +137,7 @@ print("An example of division multiplication is: " + str(4/2))
 
 
 '''
-Learn how to convert from a data type to another data type
+How to convert from a data type to another data type
 '''
 
 # to convert from any data type to string - use str() function
@@ -136,14 +155,21 @@ print(int("56") + int("22"))
 print(int(2.35))
 
 # to convert from any data type to float - use float() function
-print(float(str(2)))  # str(2) -> float('2') -> 2.0
-# print(int(str(2.35))) # this will give you an error because str(2.35) gives '2.35' and int cannot convert any non-number characters.
+print(float(str(2)))  # Conversion order str(2) -> float('2') -> 2.0
+
+# The following will give you an error because str(2.35) gives '2.35'
+# and int cannot convert any non-number characters.
+# print(int(str(2.35)))
+
 print(float(str(2.35)))
 print(float("56.33") + float("22"))
 
-
-# function evaluation order - inside out. e.g. print(float(str(55.6))) will first evaluate str(55.6) as "55.6",
-# then this will be passed to float("55.6") which will then return 55.6 which is passed to print function.
+'''
+function evaluation order - inside out. 
+e.g. print(float(str(55.6))) will first evaluate str(55.6) as "55.6",
+then this will be passed to float("55.6") which will then
+return 55.6 which is passed to print function.
+'''
 
 
 """
@@ -170,11 +196,8 @@ e.g. 2 raised to 2 - 2 * 2 = 4 (squared)
 
 print("An example of exponents: " + str(3*3*3*3*3*3*3*3*3*3))
 print("An example of exponents: " + str(3**10))
-
-
 print("An example of square of a number: " + str(9**2))
-
-
 print("The square root of 4 is: " + str(4**(1/2)))
 print("The cube root of 27 is: " + str(27**(1/3)))
 print("The fourth root of 81 is: " + str(81**(1/4)))
+print("something else")
